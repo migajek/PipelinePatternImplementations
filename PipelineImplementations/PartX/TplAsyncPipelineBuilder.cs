3958bee51c87dd6ddd60433c5667b0b497b12df3
Michal Gajek <michal.gajek@invent.us>
2019-08-28 13:13:19 +0200

    code cleanup

--- PipelineImplementations/PartX/TplAsyncPipelineBuilder.cs
+++ PipelineImplementations/PartX/TplAsyncPipelineBuilder.cs
@@ -4,49 +4,58 @@ using System.Threading.Tasks.Dataflow;
 
 namespace PipelineImplementations.PartX
 {
-    public class TplAsyncPipelineBuilder<TInput> : TplAsyncPipelineBuilder<TInput, TInput> { }
+    public class TplAsyncPipelineBuilder<TInput> : TplAsyncPipelineBuilder<TInput, TInput>
+    {
+    }
 
     public class TplAsyncPipelineBuilder<TFirstInput, TCurrentInput>
     {
         private IDataflowBlock _firstStep;
-        private IDataflowBlock _lastStep;        
+        private IDataflowBlock _lastStep;
 
         public TplAsyncPipelineBuilder() : this(null, null)
         {
-
         }
+
         private TplAsyncPipelineBuilder(IDataflowBlock firstStep, IDataflowBlock lastStep)
         {
             _firstStep = firstStep;
             _lastStep = lastStep;
         }
 
-        private void PushStep(IDataflowBlock step){
+        private void PushStep(IDataflowBlock step)
+        {
             _firstStep = _firstStep ?? step;
             _lastStep = step;
         }
 
-        public TplAsyncPipelineBuilder<TFirstInput, TOutput> AddStep<TOutput>(Func<TCurrentInput, Task<TOutput>> transformAsync)
+        public TplAsyncPipelineBuilder<TFirstInput, TOutput> AddStep<TOutput>(
+            Func<TCurrentInput, Task<TOutput>> transformAsync)
         {
             switch (_lastStep)
             {
                 case null:
-                    var initialBlock = new TransformBlock<TCurrentInput, Task<TOutput>>(async (input) => await transformAsync(input));
+                    var initialBlock =
+                        new TransformBlock<TCurrentInput, Task<TOutput>>(async (input) => await transformAsync(input));
                     PushStep(initialBlock);
                     break;
 
                 case ISourceBlock<Task<TCurrentInput>> asyncSourceBlock:
-                    var newAsyncBlock = new TransformBlock<Task<TCurrentInput>, Task<TOutput>>(async (input) => await transformAsync(await input));
+                    var newAsyncBlock =
+                        new TransformBlock<Task<TCurrentInput>, Task<TOutput>>(async (input) =>
+                            await transformAsync(await input));
                     asyncSourceBlock.LinkTo(newAsyncBlock);
                     PushStep(newAsyncBlock);
                     break;
 
                 case ISourceBlock<TCurrentInput> sourceBlock:
-                    var newBlock = new TransformBlock<TCurrentInput, Task<TOutput>>(async (input) => await transformAsync(input));
+                    var newBlock =
+                        new TransformBlock<TCurrentInput, Task<TOutput>>(async (input) => await transformAsync(input));
                     sourceBlock.LinkTo(newBlock);
                     PushStep(newBlock);
                     break;
             }
+
             return new TplAsyncPipelineBuilder<TFirstInput, TOutput>(_firstStep, _lastStep);
         }
 
